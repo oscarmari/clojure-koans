6b62e624b5cd8459e21f722c8d0d00c7a7d31b6d
Òscar Marí <oscar@oscarmari.com>
2022-01-25 14:45:44 +0100

    solve modules 18-25

--- src/koans/19_datatypes.clj
+++ src/koans/19_datatypes.clj
@@ -10,7 +10,7 @@
 (defrecord Oscar [category]
   Award
   (present [this recipient]
-    (print (str "Congratulations on your "
+    (print (str 
                 (:category this) " Oscar, "
                 recipient
                 "!"))))
@@ -18,28 +18,32 @@
 (deftype Razzie [category]
   Award
   (present [this recipient]
-    __))
+    (print (str "You're really the " (.category this) ", " recipient "... sorry."))))
 
 (meditations
   "Holding records is meaningful only when the record is worthy of you"
-  (= __ (.prize (Nobel. "peace")))
+  (= "peace" (.prize (Nobel. "peace")))
 
   "Types are quite similar"
-  (= __ (.prize (Pulitzer. "literature")))
+  (= "literature" (.prize (Pulitzer. "literature")))
 
   "Records may be treated like maps"
-  (= __ (:prize (Nobel. "physics")))
+  (= "physics" (:prize (Nobel. "physics")))
+ 
+ (comment
+   (defrecord Person [name surname age])
+   (.age (Person. "Òscar" "Marí" 37)))
 
   "While types may not"
-  (= __ (:prize (Pulitzer. "poetry")))
+  (= nil (:prize (Pulitzer. "poetry")))
 
   "Further study reveals why"
-  (= __
+  (= [true false]
      (map map? [(Nobel. "chemistry")
                 (Pulitzer. "music")]))
 
   "Either sort of datatype can define methods in a protocol"
-  (= __
+  (= "Congratulations on your Best Picture Oscar, Evil Alien Conquerors!"
      (with-out-str (present (Oscar. "Best Picture") "Evil Alien Conquerors")))
 
   "Surely we can implement our own by now"
